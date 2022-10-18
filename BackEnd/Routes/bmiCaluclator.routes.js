23b4a4bdbad72f8743d4d21189cb6af29dca9160
prabhuRV <katravthprabhu151@gmail.com>
2022-10-18 22:53:22 +0530

    backend bmi routes

--- BackEnd/Routes/bmiCaluclator.routes.js
+++ BackEnd/Routes/bmiCaluclator.routes.js
@@ -7,17 +7,21 @@ const bmiController=express.Router()
 bmiController.post("/create",async (req,res)=>
 {
     const {height,weight,user_Id}=req.body
- let bmi=weight/height
+ let bmi=(weight/height).toFixed(2)
 console.log(bmi)
     const new_bmis=new bmiModel({
-        height,weight,user_Id:user_Id
+        height,weight,user_Id:user_Id,bmi:bmi
     })
-   // await new_bmis.save()
+   await new_bmis.save()
 
     res.send({"massage":"Note new_bmis",new_bmis})
 }
 )
 
-
+bmiController.get("/", async (req, res) => {
+    const {userId} = req.body
+    const data = await bmiModel.find({userId})
+    res.send({"massage":"Result Massage",data:data})
+ })
 
 module.exports=bmiController
